7e080f187a22bc1008805029024c50496422fa75
Prawdziwy <damianes7@wp.pl>
2021-06-04 01:10:15 +0200

    Model tests, Validation

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <title></title>
+
+</head>
+
+<body>
+
+    <h1>Tutorial</h1>
+
+    <ul>
+        
+        @foreach ($projects as $project)
+
+            <li>{{ $project->title }}</li>
+        
+        @endforeach
+
+    </ul>
+
+</body>
+
+</html>
