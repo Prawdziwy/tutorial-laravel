a764fdaf3748b5d70c959e9d23878b60ef0b8141
Prawdziwy <damianes7@wp.pl>
2021-06-11 16:10:08 +0200

    Expand activity card, Migration changes, ActivityTest

--- app/Models/Activity.php
+++ app/Models/Activity.php
@@ -17,4 +17,9 @@ class Activity extends Model
     public function subject() {
         return $this->morphTo();
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
