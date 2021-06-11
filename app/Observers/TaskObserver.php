a4456e868200ad215efe583b968d08902ec7670d
Prawdziwy <damianes7@wp.pl>
2021-06-11 11:40:12 +0200

    Activity simplification of codes, Migration aditional

--- app/Observers/TaskObserver.php
+++ app/Observers/TaskObserver.php
@@ -7,10 +7,10 @@ use App\Models\Task;
 class TaskObserver
 {
     public function created(Task $task) {
-        $task->project->recordActivity('created_task');
+        $task->recordActivity('created_task');
     }
 
     public function deleted(Task $task) {
-        $task->project->recordActivity('deleted_task');
+        $task->recordActivity('deleted_task');
     }
 }
