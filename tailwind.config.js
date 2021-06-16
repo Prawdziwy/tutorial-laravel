5333c41bb16e679ca1fc0530fcf6c947e2533db6
Prawdziwy <damianes7@wp.pl>
2021-06-16 10:10:04 +0200

    Tailwind css update, Light dark style

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,17 +8,17 @@ module.exports = {
       DEFAULT: '0 0 5px 0 rgba(0, 0, 0, 0.08)',
     },
     colors: {
-      greylight: '#F5F6F9',
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      gray: colors.trueGray,
-      indigo: colors.indigo,
-      red: colors.rose,
-      yellow: colors.amber,
-      blue: '#47cdff',
-      bluelight: '#8ae2fe',
+      default: 'var(--text-default-color)',
+      accent: 'var(--text-accent-color)',
+      'accent-light': 'var(--text-accent-light-color)',
+      muted: 'var(--text-muted-color)',
+      'muted-light': 'var(--text-muted-light-color)',
+    },
+    backgroundColor: {
+      'page': 'var(--page-background-color)',
+      'card': 'var(--card-background-color)',
+      'button': 'var(--button-background-color)',
+      'header': 'var(--header-background-color)'
     },
     textColor: theme => theme('colors'),
     extend: {
@@ -28,7 +28,8 @@ module.exports = {
       textColor: {
         'grey': 'rgba(0, 0, 0, 0.4)',
         'blue': '#47cdff',
-      }
+      },
+      backgroundColor: ['active'],
     },
   },
   variants: {
